37ee12ba07afb1928ca25d850a5d8e11d969b724
Per Magne Skuseth <permagne@bvnetwork.com>
2016-09-07 10:51:41 +0200

    Secured controller

--- InspectorController.cs
+++ InspectorController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Security;
 using System.Web.Mvc;
 using EPiServer.Core;
+using EPiServer.Security;
 using EPiServer.Shell;
 
 namespace BVNetwork.ContentAreaInspector
@@ -16,6 +18,10 @@ namespace BVNetwork.ContentAreaInspector
 
         public ActionResult Index(int id)
         {
+            if (!PrincipalInfo.HasEditAccess)
+            {
+                throw new SecurityException("Access denied");
+            }
             var model = _contentAreaInspectorService.CreateModel(new ContentReference(id), null, null, 0, new List<ContentReference>());
             return View(Paths.ToResource(this.GetType(),
                 "Views/ContentAreaInspector/Index.ascx"), model);
